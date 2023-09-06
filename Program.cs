9e5978133faa4c1e75da3c8ce3685fdd20dd5708
Steve Butler <butlersteve.c@gmail.com>
2023-09-05 19:26:09 -0500

    add artist CRUD endpoints

--- Program.cs
+++ Program.cs
@@ -50,10 +50,55 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 
-app.MapGet("/api/songs", (TunapianoDbContext db) =>
+
+//Create New Artist - #1
+app.MapPost("/api/artists", (TunapianoDbContext db, Artist artist) =>
 {
-    return db.Songs.ToList();
+    db.Artists.Add(artist);
+    db.SaveChanges();
+    return Results.Created($"/api/artist/artist.Id", artist);
 });
+
+//Get All Artists - #11
+app.MapGet("/api/artists", (TunapianoDbContext db) =>
+{
+    return db.Artists.ToList();
+
+});
+
+//Update existing Artist - #4
+app.MapPut("/api/artists/{artistId}", (TunapianoDbContext db, Artist artist, int id) =>
+{
+    Artist artistToUpdate = db.Artists.SingleOrDefault(a => a.Id == id);
+    if(artistToUpdate == null)
+    {
+        return Results.NotFound();
+    }
+    artistToUpdate.Name = artist.Name;
+    artistToUpdate.Age = artist.Age;
+    artistToUpdate.Bio = artist.Bio;
+
+    db.SaveChanges();
+    return Results.Created($"/api/artists/artist.id", artist);
+
+});
+
+
+//Delete existing Artist - #17
+app.MapDelete("/api/artists/{artistId}", (TunapianoDbContext db, int id) =>
+{
+    var artist = db.Artists.SingleOrDefault(a => a.Id == id);
+    if (artist == null)
+    {
+        return Results.NotFound();
+    }
+    db.Artists.Remove(artist);
+    db.SaveChanges();
+    return Results.NoContent();
+
+});
+
+
 
 app.Run();
 
